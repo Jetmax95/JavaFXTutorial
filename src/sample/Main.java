d03451778e36a5642f177ed20247c44c4705bc99
jetmax95 <ioannis.a.angelakos@gmail.com>
2018-12-03 12:03:28 +0200

    Tutorial 13 - ChoiceBox cont. -(Having a listener whenever the user changes his option)

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -24,13 +24,14 @@ public class Main extends Application {
 
         ChoiceBox<String> choiceBox = new ChoiceBox<>();
 
+
         //getItems returns the ObservableList object which you can add items to
         choiceBox.getItems().addAll("Apples","Banana","Kiwi","Melon");
         choiceBox.setValue(choiceBox.getItems().get(0));
 
-        button.setOnAction(e ->{
-            getChoice(choiceBox);
-        });
+        //Listen for selection changes Different types of lists have different types of selection models
+        choiceBox.getSelectionModel().selectedItemProperty().addListener( (v, oldValue, newValue) -> System.out.println(newValue) );
+        // v is the property of the item, oldValue would be the previous selected item, newValue is the new item
         //Layout
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
@@ -42,10 +43,7 @@ public class Main extends Application {
         window.show();
     }
 
-    private void getChoice(ChoiceBox<String> choiceBox){
-        String food = choiceBox.getValue();
-        System.out.println("User ordered : " + food);
-    }
+
 
     public static void main(String[] args) {
         launch(args);
